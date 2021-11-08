326b82e404b9da18335643e522134cb282f1bdc1
MatiasEsp404 <unity.matias@outlook.com>
2021-11-08 18:49:35 -0300

    Actividad práctica obligatoria de Codo a Codo.

--- src/aulavirtualbackendunidad2/AulaVirtualBackEndUnidad2.java
+++ src/aulavirtualbackendunidad2/AulaVirtualBackEndUnidad2.java
@@ -1,21 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package aulavirtualbackendunidad2;
 
 import java.util.HashMap;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author unity
- */
 public class AulaVirtualBackEndUnidad2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 	
 	/**
